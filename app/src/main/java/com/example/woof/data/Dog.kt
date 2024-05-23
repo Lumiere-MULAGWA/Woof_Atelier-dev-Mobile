bb87d1884055f2c9b5a6daae8526e3d716446f07
lumiere-MULAGWA <21mb358@esisalama.org>
2024-05-23 09:53:30 +0200

    ajout de la class Dog

--- app/src/main/java/com/example/woof/data/Dog.kt
+++ app/src/main/java/com/example/woof/data/Dog.kt
@@ -1,98 +1,31 @@
 package com.example.woof.data
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.example.woof.R
+
 class Dog {
-
     /**
-     * Composable that displays an app bar and a list of dogs.
+     * A data class to represent the information presented in the dog card
      */
-    @Composable
-    fun WoofApp() {
-        LazyColumn {
-            items(dogs) {
-                DogItem(dog = it)
-            }
-        }
-    }
+    data class Dog(
+        @DrawableRes val imageResourceId: Int,
+        @StringRes val name: Int,
+        val age: Int,
+        @StringRes val hobbies: Int
+    )
 
-    /**
-     * Composable that displays a list item containing a dog icon and their information.
-     *
-     * @param dog contains the data that populates the list item
-     * @param modifier modifiers to set to this composable
-     */
-    @Composable
-    fun DogItem(
-        dog: Dog,
-        modifier: Modifier = Modifier
-    ) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(R.dimen.padding_small))
-        ) {
-            DogIcon(dog.imageResourceId)
-            DogInformation(dog.name, dog.age)
-        }
-    }
+    val dogs = listOf(
+        Dog(R.drawable.koda, R.string.dog_name_1, 2, R.string.dog_description_1),
+        Dog(R.drawable.lola, R.string.dog_name_2, 16, R.string.dog_description_2),
+        Dog(R.drawable.frankie, R.string.dog_name_3, 2, R.string.dog_description_3),
+        Dog(R.drawable.nox, R.string.dog_name_4, 8, R.string.dog_description_4),
+        Dog(R.drawable.faye, R.string.dog_name_5, 8, R.string.dog_description_5),
+        Dog(R.drawable.bella, R.string.dog_name_6, 14, R.string.dog_description_6),
+        Dog(R.drawable.moana, R.string.dog_name_7, 2, R.string.dog_description_7),
+        Dog(R.drawable.tzeitel, R.string.dog_name_8, 7, R.string.dog_description_8),
+        Dog(R.drawable.leroy, R.string.dog_name_9, 4, R.string.dog_description_9)
+    )
 
-    /**
-     * Composable that displays a photo of a dog.
-     *
-     * @param dogIcon is the resource ID for the image of the dog
-     * @param modifier modifiers to set to this composable
-     */
-    @Composable
-    fun DogIcon(
-        @DrawableRes dogIcon: Int,
-        modifier: Modifier = Modifier
-    ) {
-        Image(
-            modifier = modifier
-                .size(dimensionResource(R.dimen.image_size))
-                .padding(dimensionResource(R.dimen.padding_small)),
-            painter = painterResource(dogIcon),
 
-            // Content Description is not needed here - image is decorative, and setting a null content
-            // description allows accessibility services to skip this element during navigation.
-
-            contentDescription = null
-        )
-    }
-
-    /**
-     * Composable that displays a dog's name and age.
-     *
-     * @param dogName is the resource ID for the string of the dog's name
-     * @param dogAge is the Int that represents the dog's age
-     * @param modifier modifiers to set to this composable
-     */
-    @Composable
-    fun DogInformation(
-        @StringRes dogName: Int,
-        dogAge: Int,
-        modifier: Modifier = Modifier
-    ) {
-        Column(modifier = modifier) {
-            Text(
-                text = stringResource(dogName),
-                modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
-            )
-            Text(
-                text = stringResource(R.string.years_old, dogAge),
-            )
-        }
-    }
-
-    Replacer la section de l'anotation preview par le code suivant
-
-    /**
-     * Composable that displays what the UI of the app looks like in light theme in the design tab.
-     */
-    @Preview
-    @Composable
-    fun WoofPreview() {
-        WoofTheme(darkTheme = false) {
-            WoofApp()
-        }
-    }
 }
